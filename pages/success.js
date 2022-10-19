e9ad5414d757efa2858901c79a7b2b9880f84877
Aarushi Shanker <aarushishanker3108@gmail.com>
2022-10-19 19:57:12 +0530

    Component name changed

--- pages/success.js
+++ pages/success.js
@@ -5,7 +5,7 @@ import {BsBagCheckFill} from 'react-icons/bs'
 import { useStateContext } from '../context/StateContext'
 import { runFireworks } from '../lib/utils'
 
-const success = () => {
+const Success = () => {
     const {setcartItems,settotalPrice,settotalQuantities}=useStateContext();
 
     useEffect(()=>{
@@ -38,4 +38,4 @@ const success = () => {
     )
 }
 
-export default success
+export default Success
